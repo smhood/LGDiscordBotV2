5fcaed7af495acec98de1084a2b6f993afd6f666
smhood <Hoodster92@gmail.com>
2018-02-20 17:33:20 -0600

    prefix?

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.content === 'ping') {
-    message.reply('pong');
+    message.reply('pong' + process.env.PREFIX);
   }
 });
 
