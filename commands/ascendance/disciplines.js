21f67bb2f44273909cd5f902d4bb67cf76398343
smhood <Hoodster92@gmail.com>
2018-02-25 11:36:57 -0600

    whats going on?

--- commands/ascendance/disciplines.js
+++ commands/ascendance/disciplines.js
@@ -1,7 +1,7 @@
 const { Command } = require('discord.js-commando');
 const { getDatabase } = require('../../database/mongoDb');
 
-module.exports = class GetUserActivity extends Command {
+module.exports = class GetDisciplines extends Command {
   constructor(client) {
       super(client, {
           name: 'disciplines',
